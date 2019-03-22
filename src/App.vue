af769d5caf44c924b1e82be0bfe8ac1358316511
MyTimebu <llf200103@gmail.com>
2019-03-22 08:28:38 +0800

    一

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <!-- <router-link to="/"></router-link> -->
+      <!-- <router-link to="/about">About</router-link> -->
     <router-view/>
   </div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+ width: 100%;
+ height: 100%;
 }
 
 #nav a {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="center">
+      <div class="tou"><img src="../assets/1.png"></div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
+
+<style>
+  .home{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    position: relative;
+    background-color: #006ae2
+  }
+  .center{
+    width: 500px;
+    height: 300px;
+    background-color: #fff;
+  }
+  .tou{
+    width: 100px;
+    position: absolute;
+    left: 620px;
+    top:130px;
+    height: 100px;
+    border: 10px solid #fff;
+    border-radius: 50%;
+    box-shadow: 1px 10px 15px 1px;
+  }
+  .tou>img{
+    width: 100%;
+    height: 100%;
+    border-radius: 50%
+  }
+</style>
